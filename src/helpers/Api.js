29959b31592254cc1b76d905a623005b7d718acb
isanmh <1157050074@student.uinsgd.co.id>
2022-09-29 11:00:48 +0700

    Fix Rest Api

--- src/helpers/Api.js
+++ src/helpers/Api.js
@@ -1,10 +1,12 @@
 const API = 'https://reqres.in/api/users/1';
-const localAPI = 'http://10.0.2.2:3000/users';
-// const localAPI = 'http://localhost:3000/users';
-// const localAPI = 'http://192.168.95.152:3000/users';
+const localAPI = 'http://10.0.2.2:3000/contacts';
+// const localAPI = 'http://localhost:3000/contacts';
+// const localAPI = 'http://192.168.95.152:3000/contacts';
 // const localAPI =
 //   'https://my-json-server.typicode.com/metaliccode/api-pwa/users';
 
 // const localAPI = 'http://10.0.2.2:8000/api/contacts';
+// const localAPI = 'http://192.168.95.152:8000/contacts';
+// const localAPI = 'http://localhost:8000/contacts';
 
 export {API, localAPI};
